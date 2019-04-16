9e22c559578190152f40b59c9c86dab58ebe9ae9
Ting-Yu Lin <tylin@hpc.psy.ntu.edu.tw>
2019-04-16 22:06:58 +0800

    cogdatatype function

---
+++ R/cog_datatype.R
@@ -0,0 +1,22 @@
+cog_datatype <- function(df, factorlist = list(), numericlist = list()){
+  #common data datatype
+  df <- transform(df,
+                  Subject = as.factor(Subject),
+                  Gender = as.factor(Gender),
+                  Age = as.numeric(Age),
+                  Education = as.factor(Education),
+                  Hand = as.factor(Hand),
+                  Seed = as.factor(Seed))
+
+  #input factor list
+  for (i in factorlist){
+    df[[i]] <- factor(df[[i]])
+  }
+
+  #input numeric list
+  for (i in numericlist){
+    df[[i]] <- as.numeric(df[[i]])
+  }
+
+  return(df)
+}
