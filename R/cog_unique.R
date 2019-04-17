17abc282524966671f08fc2363e95ac2683488bc
Ting-Yu Lin <tylin@hpc.psy.ntu.edu.tw>
2019-04-17 22:35:03 +0800

    cog_unique by task

---
+++ R/cog_unique.R
@@ -0,0 +1,21 @@
+cog_unique <- function(df, Task){
+  if (Task == "SRTCRT"){
+    summary_string <- "summarise(.,
+    SRT_mrt = unique(SRT_mrt),
+    SRT_vrt = unique(SRT_vrt),
+    CRT_mrt = unique(CRT_mrt),
+    CRT_vrt = unique(CRT_vrt),
+    SRT_mac = unique(SRT_mac),
+    SRT_vac = unique(SRT_vac),
+    CRT_mac = unique(CRT_mac),
+    CRT_vac = unique(CRT_vac)
+    )"
+
+  }
+
+  tmpdf <- df %>%
+    group_by(Subject, Gender, Age, Education, Hand, Seed) %>%
+    eval(parse(text = summary_string), envir = .)
+
+  return(tmpdf)
+}
