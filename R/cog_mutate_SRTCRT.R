175654cf28e6a6f3b9a0970388f97aaddfa7d137
Ting-Yu Lin <tylin@hpc.psy.ntu.edu.tw>
2019-04-17 21:16:35 +0800

    mutate revisted

--- R/cog_mutate_SRTCRT.R
+++ R/cog_mutate_SRTCRT.R
@@ -1,12 +1,17 @@
-cog_mutate_SRTCRT <- function(df){
-  tmp <- df %>%
+cog_mutate_SRTCRT <- function (df)
+{
+  tmpdf <- df %>%
     group_by(Subject, Gender, Age, Education, Hand, Seed) %>%
-    filter(Block != 0) %>%
+    # filter(Block != 0) %>%
     mutate(
-      SRT_mac  = mean(Accuracy[StimulusLocation == 5]),
-      SRT_vac = var(Accuracy[StimulusLocation == 5]),
-      CRT_mac = mean(Accuracy[StimulusLocation != 5]),
-      CRT_vac = var(Accuracy[StimulusLocation != 5])
+      SRT_mrt = mean(RT[StimulusLocation == 5 & Block != 0 & Accuracy == 1]),
+      SRT_vrt = var(RT[StimulusLocation == 5 & Block != 0 & Accuracy == 1]),
+      CRT_mrt = mean(RT[StimulusLocation != 5 & Block != 0 & Accuracy == 1]),
+      CRT_vrt = var(RT[StimulusLocation != 5 & Block != 0 & Accuracy == 1]),
+      SRT_mac = mean(Accuracy[StimulusLocation == 5 & Block != 0]),
+      SRT_vac = var(Accuracy[StimulusLocation == 5 & Block != 0]),
+      CRT_mac = mean(Accuracy[StimulusLocation != 5 & Block != 0]),
+      CRT_vac = var(Accuracy[StimulusLocation != 5 & Block != 0])
     )
-  return(tmp)
+  return(tmpdf)
 }
