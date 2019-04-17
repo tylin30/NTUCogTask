e4ae1af5094ba7694e9b1b974a7145f69dfb5a4a
Ting-Yu Lin <tylin@hpc.psy.ntu.edu.tw>
2019-04-17 22:23:28 +0800

    cog_mutate by pass Task as argu

---
+++ R/cog_mutate.R
@@ -0,0 +1,12 @@
+cog_mutate <- function (df, Task)
+{
+  if (Task == "SRTCRT"){
+    mutate_string <- "mutate(., SRT_mrt = mean(RT[StimulusLocation == 5 & Block != 0 & Accuracy == 1]),SRT_vrt = var(RT[StimulusLocation == 5 & Block != 0 & Accuracy == 1]),CRT_mrt = mean(RT[StimulusLocation != 5 & Block != 0 & Accuracy == 1]),CRT_vrt = var(RT[StimulusLocation != 5 & Block != 0 & Accuracy == 1]),SRT_mac = mean(Accuracy[StimulusLocation == 5 & Block != 0]),SRT_vac = var(Accuracy[StimulusLocation == 5 & Block != 0]),CRT_mac = mean(Accuracy[StimulusLocation != 5 & Block != 0]),CRT_vac = var(Accuracy[StimulusLocation != 5 & Block != 0]))"
+  }
+
+  tmpdf <- df %>%
+    group_by(Subject, Gender, Age, Education, Hand, Seed) %>%
+    eval(parse(text = mutate_string), envir = .)
+
+  return(tmpdf)
+}
