63a5e6d80b58fee52d90edc684aacf207d148ddb
Ting-Yu Lin <tylin@hpc.psy.ntu.edu.tw>
2019-04-18 15:36:07 +0800

    cog_rbindobs all task shortcut mapping

--- R/cog_rbindobs.R
+++ R/cog_rbindobs.R
@@ -8,6 +8,45 @@ cog_rbindobs <- function(path, Task){
   else if (Task == "MAO"){
     Regex_pattern <- ".*Memory_of_Association_Object.*.csv"
   }
+  else if (Task == "DMS"){
+    Regex_pattern <- ".*Delayed_Matching_to_Sample.*.csv"
+  }
+  else if (Task == "DR"){
+    Regex_pattern <- ".*Delayed_Response_Task.*.csv"
+  }
+  else if (Task == "SM"){
+    Regex_pattern <- ".*Spatial_Memory_Task.*.csv"
+  }
+  else if (Task == "RMS"){
+    Regex_pattern <- ".*Running_Memory_of_Symbols.*.csv"
+  }
+  else if (Task == "RMO"){
+    Regex_pattern <- ".*Running_Memory_of_Objects.*.csv"
+  }
+  else if (Task == "RML"){
+    Regex_pattern <- ".*Running_Memory_of_Locations.*.csv"
+  }
+  else if (Task == "SST"){
+    Regex_pattern <- ".*Stop_Signal_Task.*.csv"
+  }
+  else if (Task == "Sp"){
+    Regex_pattern <- ".*Stroop*.csv"
+  }
+  else if (Task == "As"){
+    Regex_pattern <- ".*Antisaccade_Arrow*.csv"
+  }
+  else if (Task == "CTT"){
+    Regex_pattern <- ".*Color_Trail_Test*.csv"
+  }
+  else if (Task == "Fg"){
+    Regex_pattern <- ".*Figure_Task*.csv"
+  }
+  else if (Task == "HF"){
+    Regex_pattern <- ".*Hearts_and_Flowers*.csv"
+  }
+  else if (Task == "RS"){
+    Regex_pattern <- ".*Rotation_Span_Task*.csv"
+  }
   else{
     Regex_pattern <- Task
   }
