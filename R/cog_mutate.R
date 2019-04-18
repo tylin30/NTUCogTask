3d6b09e23a43bcc8b80c154cb77d7792a08a7249
Ting-Yu Lin <tylin@hpc.psy.ntu.edu.tw>
2019-04-18 13:07:55 +0800

    cog_mutate add DMS

--- R/cog_mutate.R
+++ R/cog_mutate.R
@@ -3,6 +3,9 @@ cog_mutate <- function (df, Task)
   if (Task == "SRTCRT"){
     mutate_string <- "mutate(., SRT_mrt = mean(RT[StimulusLocation == 5 & Block != 0 & Accuracy == 1]),SRT_vrt = var(RT[StimulusLocation == 5 & Block != 0 & Accuracy == 1]),CRT_mrt = mean(RT[StimulusLocation != 5 & Block != 0 & Accuracy == 1]),CRT_vrt = var(RT[StimulusLocation != 5 & Block != 0 & Accuracy == 1]),SRT_mac = mean(Accuracy[StimulusLocation == 5 & Block != 0]),SRT_vac = var(Accuracy[StimulusLocation == 5 & Block != 0]),CRT_mac = mean(Accuracy[StimulusLocation != 5 & Block != 0]),CRT_vac = var(Accuracy[StimulusLocation != 5 & Block != 0]))"
   }
+  else if (Task == "DMS"){
+    mutate_string <- "mutate(.)"
+  }
 
   tmpdf <- df %>%
     group_by(Subject, Gender, Age, Education, Hand, Seed) %>%
