052688979a93071ee03c73feceefa3c95445bd28
tylin <tylin.amice@gmail.com>
2019-05-21 23:51:04 +0800

    fix little problen

--- R/cog_qqplot.R
+++ R/cog_qqplot.R
@@ -1,5 +1,5 @@
 cog_qqplot <- function(df, title_label = ""){
-  if(title_label != ""){title_label <- paste0("("title_label,")"}
+  if(title_label != ""){title_label <- paste0("(",title_label,")"}
   for (i in 1:ncol(df)){
     if (is.numeric(df[[i]])){
       plot <- ggplot(selected_rt, aes(sample=selected_rt[[i]])) +
