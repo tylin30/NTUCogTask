577b157c397ada6b9b26203b6f529d934704ae1d
tylin <tylin.amice@gmail.com>
2019-05-22 00:02:01 +0800

    test for error

--- R/multi_join.R
+++ R/multi_join.R
@@ -1,5 +1,5 @@
-multi_join <- function(list_of_loaded_data, join_func, ...){
-  require("dplyr")
-  output <- Reduce(function(x, y) {join_func(x, y, ...)}, list_of_loaded_data)
-  return(output)
-}
+# multi_join <- function(list_of_loaded_data, join_func, ...){
+#   require("dplyr")
+#   output <- Reduce(function(x, y) {join_func(x, y, ...)}, list_of_loaded_data)
+#   return(output)
+# }
