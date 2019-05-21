0d85080b3c8ef2bad76b627c715e26aec679537f
tylin <tylin.amice@gmail.com>
2019-05-22 00:04:47 +0800

    test2

--- R/cog_joinall.R
+++ R/cog_joinall.R
@@ -1,11 +1,11 @@
-# cog_joinall <- function(dflist = list(srtcrt_df, ma_df, mao_df, dms_df, dr_df,
-#                                       sm_df, rms_df, rmo_df, rml_df, sst_df,
-#                                       as_df, ctt_df, fg_df, hf_df, rs_df,
-#                                       sp_df)){
-#
-#   #temp method for join (due to typo error from experimenter)
-#   delete <- c("Gender", "Age", "Education", "Hand", "Seed")
-#   tmp <- lapply(dflist, function(x) {x <- x[, !(colnames(x) %in% delete), drop=FALSE] })
-#
-#   all_df <- multi_join(tmp, full_join)
-# }
+cog_joinall <- function(dflist = list(srtcrt_df, ma_df, mao_df, dms_df, dr_df,
+                                      sm_df, rms_df, rmo_df, rml_df, sst_df,
+                                      as_df, ctt_df, fg_df, hf_df, rs_df,
+                                      sp_df)){
+
+  #temp method for join (due to typo error from experimenter)
+  delete <- c("Gender", "Age", "Education", "Hand", "Seed")
+  tmp <- lapply(dflist, function(x) {x <- x[, !(colnames(x) %in% delete), drop=FALSE] })
+
+  all_df <- multi_join(tmp, full_join)
+}
--- R/cog_qqplot.R
+++ R/cog_qqplot.R
@@ -1,14 +1,14 @@
-cog_qqplot <- function(df, title_label = ""){
-  if(title_label != ""){title_label <- paste0("(",title_label,")")
-  for (i in 1:ncol(df)){
-    if (is.numeric(df[[i]])){
-      plot <- ggplot(selected_rt, aes(sample=selected_rt[[i]])) +
-        geom_qq(distribution = qnorm) +
-        geom_qq_line(col = "blue") +
-        ylab(names(selected_rt)[i]) +
-        ggtitle(paste(names(selected_rt)[i],"Q-Qplot", title_label)) +
-        theme(plot.title = element_text(hjust = 0.5))
-      print(plot)
-    }
-  }
-}
+# cog_qqplot <- function(df, title_label = ""){
+#   if(title_label != ""){title_label <- paste0("(",title_label,")")
+#   for (i in 1:ncol(df)){
+#     if (is.numeric(df[[i]])){
+#       plot <- ggplot(selected_rt, aes(sample=selected_rt[[i]])) +
+#         geom_qq(distribution = qnorm) +
+#         geom_qq_line(col = "blue") +
+#         ylab(names(selected_rt)[i]) +
+#         ggtitle(paste(names(selected_rt)[i],"Q-Qplot", title_label)) +
+#         theme(plot.title = element_text(hjust = 0.5))
+#       print(plot)
+#     }
+#   }
+# }
