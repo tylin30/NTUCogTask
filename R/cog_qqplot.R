bb2ecefec9e5be0fb3ed531060b5c5e4a376373b
tylin <tylin.amice@gmail.com>
2019-05-21 23:38:07 +0800

    qqplot function add title label

--- R/cog_qqplot.R
+++ R/cog_qqplot.R
@@ -1,11 +1,11 @@
-cog_qqplot <- function(df){
+cog_qqplot <- function(df, title_label){
   for (i in 1:ncol(df)){
     if (is.numeric(df[[i]])){
       plot <- ggplot(selected_rt, aes(sample=selected_rt[[i]])) +
         geom_qq(distribution = qnorm) +
         geom_qq_line(col = "blue") +
         ylab(names(selected_rt)[i]) +
-        ggtitle(paste(names(selected_rt)[i],"Q-Qplot")) +
+        ggtitle(paste(,names(selected_rt)[i], title_label,"Q-Qplot")) +
         theme(plot.title = element_text(hjust = 0.5))
       print(plot)
     }
