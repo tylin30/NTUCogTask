d6f52bc6bdb9dafaae34f669743a59f61d604666
tylin <tylin.amice@gmail.com>
2019-06-15 08:03:00 +0800

    cog_qqplot

--- R/cog_qqplot.R
+++ R/cog_qqplot.R
@@ -2,17 +2,36 @@ cog_qqplot <- function(df, title_label = ""){
 
   if(title_label != ""){
     title_label <- paste0("(",title_label,")")
-    }
+  }
 
   for (i in 1:ncol(df)){
     if (is.numeric(df[[i]])){
-      plot <- ggplot(selected_rt, aes(sample=selected_rt[[i]])) +
+      plot <- ggplot(df, aes(sample=df[[i]])) +
         geom_qq(distribution = qnorm) +
         geom_qq_line(col = "blue") +
-        ylab(names(selected_rt)[i]) +
-        ggtitle(paste(names(selected_rt)[i],"Q-Qplot", title_label)) +
+        ylab(names(df)[i]) +
+        ggtitle(paste(names(df)[i],"Q-Qplot", title_label)) +
         theme(plot.title = element_text(hjust = 0.5))
       print(plot)
     }
   }
 }
+
+# cog_qqplot <- function(df, title_label = ""){
+#
+#   if(title_label != ""){
+#     title_label <- paste0("(",title_label,")")
+#     }
+#
+#   for (i in 1:ncol(df)){
+#     if (is.numeric(df[[i]])){
+#       plot <- ggplot(selected_rt, aes(sample=selected_rt[[i]])) +
+#         geom_qq(distribution = qnorm) +
+#         geom_qq_line(col = "blue") +
+#         ylab(names(selected_rt)[i]) +
+#         ggtitle(paste(names(selected_rt)[i],"Q-Qplot", title_label)) +
+#         theme(plot.title = element_text(hjust = 0.5))
+#       print(plot)
+#     }
+#   }
+# }
