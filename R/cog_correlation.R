bca722852fb5325e02f4fb2327894cb514269f63
tylin <tylin.amice@gmail.com>
2019-06-23 13:16:13 +0800

    fix cog_correlation removesheet

--- R/cog_correlation.R
+++ R/cog_correlation.R
@@ -2,11 +2,11 @@ cog_correlation <- function(cormatrix, alpha = 0.05, filename = "cor", path = ".
   cor <- as_tibble(cormatrix$r)
   cor_p <- as_tibble(cormatrix$P)
   cor_remove <- cor
-  
+
   for( i in c(1:ncol(cor)) ){
     for ( j in c(1:nrow(cor))){
       if (is.na(cor_p[[i]][j])){
-        
+
       }else if (cor_p[[i]][j] > alpha){
         cor_remove[[i]][j] <- ""
       }
@@ -18,9 +18,9 @@ cog_correlation <- function(cormatrix, alpha = 0.05, filename = "cor", path = ".
   write.xlsx2(cormatrix$r, file = paste0(path, "/", filename,".xlsx"),
               sheetName = "cor", append = FALSE)
   # Add a second data set in a new worksheet
-  write.xlsx2(cormatrix$P, file = paste0(path, "/", filename,".xlsx"), 
+  write.xlsx2(cormatrix$P, file = paste0(path, "/", filename,".xlsx"),
               sheetName="cor_p", append=TRUE)
   # Add a third data set
-  write.xlsx2(cormatrix$r, file = paste0(path, "/", filename,".xlsx"),
+  write.xlsx2(cor_remove, file = paste0(path, "/", filename,".xlsx"),
               sheetName="cor_remove", append=TRUE)
 }
