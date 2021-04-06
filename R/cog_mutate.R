c3b50c08a39e825cad9cc94a29e41f0e5ff55ed1
Ting-Yu Lin <tylin.amice@gmail.com>
2021-04-06 21:18:29 +0800

    cog_mutate.R fix last_10stopACC

--- R/cog_mutate.R
+++ R/cog_mutate.R
@@ -87,8 +87,9 @@ cog_mutate <- function (df, Task)
     formalgodf <- df %>% filter(Block == 1, Condition == 'go') %>% select(Trial, Condition, Block)
     last30_trial = as.numeric(tail(unique(formalgodf$Trial),30)[1])
     
-    stopdf <- df %>% filter(Block == 1, Condition == 'stop') %>% select(Condition, Block, Accuracy)
-    last10_stopACC = mean(as.numeric(tail(stopdf$Accuracy,10)))
+    stopdf <- df %>% filter(Block == 1, Condition == 'stop') %>% select(Trial, Condition, Block)
+    last10_trial = as.numeric(tail(unique(stopdf$Trial),10)[1])
+
     mutate_string <- paste0(mutate_string, "%>%",
                             "mutate(.,",
                             Task, "_last30rt = mean(RT_ro[Block == 1 & Accuracy == 1 & Condition == 'go' & as.numeric(Trial) >= last30_trial], na.rm = TRUE),",
@@ -96,7 +97,7 @@ cog_mutate <- function (df, Task)
                             Task, "_int_accadjust_no05 = (SST_int/(SST_stopac_no05/0.5))*100, ",
                             Task, "_index =", Task, "_last30rt", "/", Task, "_int_accadjust,",
                             Task, "_index_no05 =", Task, "_last30rt", "/", Task, "_int_accadjust_no05,",
-                            Task, "_last10_stopACC = last10_stopACC",
+                            Task, "_last10_stopACC = mean(Accuracy[Block == 1 & Condition == 'stop' & as.numeric(Trial) >= last10_trial])",
                             ")"
     )
   }
