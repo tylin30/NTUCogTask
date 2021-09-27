b099f899a21ea8e41133ca3f1782b72a14b6dd8c
Ting-Yu Lin <tylin.amice@gmail.com>
2021-09-28 00:07:47 +0800

    Update cog_addnooutlier "HF" groupby pure&mix

--- R/cog_addnooutlier.R
+++ R/cog_addnooutlier.R
@@ -92,6 +92,20 @@ cog_addnooutlier <- function(df, Task, range = 2.5){
       )
       )
   }
+  
+   # not duplicate, group by pure, mix
+  if(Task == "HF"){
+    df <- df %>%
+      mutate(Group = ifelse((Condition == 1 | Condition == 2), "pure", ifelse((Condition == 3 | Condition == 4), 'mix', 'practice'))) %>%
+      group_by(Subject, Group) %>%
+      mutate(RT_condition_ro = ifelse(
+        RT >= mean(RT[ Accuracy == 1]) + range*sd(RT[ Accuracy == 1]) |
+          RT <= mean(RT[Accuracy == 1]) - range*sd(RT[Accuracy == 1])|
+          RT <= min_rt,
+        NA, RT
+      )
+      )
+  }
 
   if(Task == "Sp"){
     df <- df %>%
